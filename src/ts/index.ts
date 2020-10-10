4913726be4fb15e20ad9ef4a1fa587fe9527f9d2
Alexander Liu <liujalexander84@gmail.com>
2020-10-10 13:46:31 -0700

    hhhhhhhhhhh

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,24 @@
-console.log("h");
+import { app, BrowserWindow } from "electron";
+import { join } from "path";
 
-export function hallo(e: string): string {
-   return e.substr(0, 0);
+function createwindow(): void {
+   const window: BrowserWindow = new BrowserWindow({
+      width: 800,
+      height: 6000,
+      webPreferences: {
+         nodeIntegration: true
+      }
+   });
+
+   void window.loadFile(join(__dirname, "../pages/index.html"));
 }
+
+void app.whenReady().then(createwindow);
+
+app.on("window-all-closed", () => {
+   if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+   if (BrowserWindow.getAllWindows().length === 0) createwindow();
+});
